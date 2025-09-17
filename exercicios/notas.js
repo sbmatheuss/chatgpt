43b239703ecdbd34b5ccd9059e29a997cae5c3b3
Matheus Braga <sbmatheus20@gmail.com>
2025-09-17 19:50:52 -0300

    Classificação de Notas

---
+++ exercicios/notas.js
@@ -0,0 +1,8 @@
+var n = 8
+if (n < 5) {
+  console.log(`Nota: ${n} Você está Reprovado!`)
+} else if (n >= 5 && n < 7) {
+  console.log(`Nota: ${n} Você está em Recuperação!`)
+} else {
+  console.log(`Nota: ${n} Você está Aprovado!`)
+}
